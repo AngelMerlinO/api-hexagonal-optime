ce2486580e1e1d9055f711e50788b9eb3c9eea0c
José Angel Ortega Merlin <105680717+AngelMerlinO@users.noreply.github.com>
2024-10-21 15:17:56 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,14 @@
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config.database import Base
+from config.test_database import Base, engine, SessionLocal
 
 @pytest.fixture(scope='function')
 def db_session():
-    # Configuración de la base de datos en memoria para pruebas
-    engine = create_engine("sqlite:///:memory:")
-    Session = sessionmaker(bind=engine)
-
-    # Crea todas las tablas en la base de datos en memoria
+    # Crear todas las tablas en la base de datos en memoria para las pruebas
     Base.metadata.create_all(engine)
 
-    session = Session()
+    session = SessionLocal()
     yield session
 
-    # Limpia la base de datos después de cada prueba
+    # Limpiar la base de datos después de cada prueba
     session.close()
     Base.metadata.drop_all(engine)
