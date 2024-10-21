4ff1656ae50cc1abf6cea010618cf4b2bdbaa800
José Angel Ortega Merlin <105680717+AngelMerlinO@users.noreply.github.com>
2024-10-21 15:10:28 -0600

    Update database.py

--- config/database.py
+++ config/database.py
@@ -6,21 +6,26 @@ from dotenv import load_dotenv
 # Cargar las variables del archivo .env
 load_dotenv()
 
-# Leer las variables de entorno
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT')
+# Definir Base
+Base = declarative_base()
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Verificar si estamos ejecutando pruebas
+if os.getenv("PYTEST_CURRENT_TEST"):
+    # Usar SQLite en memoria para las pruebas
+    DATABASE_URL = "sqlite:///:memory:"
+else:
+    # Leer las variables de entorno para la configuración de MySQL
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_PORT = os.getenv('DB_PORT')
+
+    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Crear el motor y sesión de SQLAlchemy
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Definir Base
-Base = declarative_base()  # Agregado
 
 def get_db():
     db = SessionLocal()
