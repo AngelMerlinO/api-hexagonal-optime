8325fb15497b60b3004b14fa97c4a315b55a1e71
José Angel Ortega Merlin <105680717+AngelMerlinO@users.noreply.github.com>
2024-10-21 15:24:58 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,25 @@
 import pytest
-from config.test_database import Base, engine, SessionLocal
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config.database import Base
+from config.database import SessionLocal, engine as default_engine
 
 @pytest.fixture(scope='function')
-def db_session():
-    # Crear todas las tablas en la base de datos en memoria para las pruebas
-    Base.metadata.create_all(engine)
+def db_session(monkeypatch):
+    # Crear el engine para usar SQLite en memoria para las pruebas
+    test_engine = create_engine("sqlite:///:memory:")
 
-    session = SessionLocal()
+    # Crear todas las tablas en la base de datos en memoria
+    Base.metadata.create_all(test_engine)
+
+    # Mockear la conexión del motor de SQLAlchemy a SQLite
+    session = sessionmaker(bind=test_engine)()
+
+    # Parchar la sesión local para usar la sesión SQLite en lugar de MySQL
+    monkeypatch.setattr('config.database.SessionLocal', lambda: session)
+
     yield session
 
     # Limpiar la base de datos después de cada prueba
     session.close()
-    Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(test_engine)
