c1480ec41e730e0848d6a4546f6f1feac39e486f
Jose Angel Ortega Merlin <angelortegamerlin@gmail.com>
2024-10-23 21:19:03 -0600

    add used cors and certificate ssl

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+import os
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from src.routes import router
+
+# Cargar las variables del archivo .env
+load_dotenv()
 
 app = FastAPI()
 
-# Registrar todas las rutas de la aplicación
+# Configuración de CORS (si es necesario)
+origins = ["http://localhost", "https://localhost"]  # Ajusta según necesites
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Registrar las rutas
 app.include_router(router)
+
+if __name__ == "__main__":
+    # Leer las variables de entorno
+    port = int(os.getenv("PORT", 8000))
+    use_ssl = os.getenv("USE_SSL", "False") == "True"
+    ssl_certfile = os.getenv("SSL_CERTFILE")
+    ssl_keyfile = os.getenv("SSL_KEYFILE")
+
+    # Ejecutar con SSL si está habilitado en el entorno
+    if use_ssl and ssl_certfile and ssl_keyfile:
+        uvicorn.run("main:app", host="0.0.0.0", port=port, ssl_certfile=ssl_certfile, ssl_keyfile=ssl_keyfile)
+    else:
+        # Ejecutar sin SSL si no está configurado
+        uvicorn.run("main:app", host="0.0.0.0", port=port)
