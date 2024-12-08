5967a6390612e7ce60cee4da7ed33a78bd265db6
Jose Angel Ortega Merlin <angelortegamerlin@gmail.com>
2024-12-07 18:52:53 -0600

    add serveless optime in repository

---
+++ Optime-azure-fuctions/data.py
@@ -0,0 +1,380 @@
+mapa_curricular = [
+    {
+        'nombre': 'Inglés I',
+        'cuatrimestre': 1,
+        'seriacion': [1,1,1,1,1,1,1,1],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés II',
+        'cuatrimestre': 2,
+        'seriacion': [1,1,1,1,1,1,1,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés III',
+        'cuatrimestre': 3,
+        'seriacion': [1,1,1,1,1,1,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés IV',
+        'cuatrimestre': 4,
+        'seriacion': [1,1,1,1,1,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés V',
+        'cuatrimestre': 5,
+        'seriacion': [1,1,1,1,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés VI',
+        'cuatrimestre': 6,
+        'seriacion': [1,1,1,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés VII',
+        'cuatrimestre': 7,
+        'seriacion': [1,1,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés VIII',
+        'cuatrimestre': 8,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inglés IX',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Química Básica',
+        'cuatrimestre': 1,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 9
+    },
+    {
+        'nombre': 'Álgebra Lineal',
+        'cuatrimestre': 1,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 9
+    },
+    {
+        'nombre': 'Fundamentos de Computación',
+        'cuatrimestre': 1,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 9
+    },
+    {
+        'nombre': 'Algoritmos',
+        'cuatrimestre': 1,
+        'seriacion': [2,2,1,1,1,4,2,3],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Matemáticas Discretas',
+        'cuatrimestre': 1,
+        'seriacion': [None,None,None,None,None,None,1,None],
+        'holgura': 7
+    },
+    {
+        'nombre': 'Expresión Oral y Escrita I',
+        'cuatrimestre': 1,
+        'seriacion': [None,None,None,None,None,None,None,1],
+        'holgura': 8
+    },
+    {
+        'nombre': 'Desarrollo Humano y Valores',
+        'cuatrimestre': 2,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Cálculo Diferencial',
+        'cuatrimestre': 2,
+        'seriacion': [1,1,1,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Programación Orientada a Objetos',
+        'cuatrimestre': 2,
+        'seriacion': [1,1,1,1,4,2,3,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Estructuras de Datos',
+        'cuatrimestre': 2,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Ingeniería de Software Asistida por Computadora',
+        'cuatrimestre': 2,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 8
+    },
+    {
+        'nombre': 'Procesos de Desarrollo de Software',
+        'cuatrimestre': 2,
+        'seriacion': [1,None,1,1,1,2,1,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Inteligencia Emocional y Manejo de Conflictos',
+        'cuatrimestre': 3,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 7
+    },
+    {
+        'nombre': 'Cálculo Integral',
+        'cuatrimestre': 3,
+        'seriacion': [1,1,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Programación Visual',
+        'cuatrimestre': 3,
+        'seriacion': [1,1,1,4,2,3,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Estructuras de Datos Avanzadas',
+        'cuatrimestre': 3,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 7
+    },
+    {
+        'nombre': 'Fundamentos de Bases de Datos',
+        'cuatrimestre': 3,
+        'seriacion': [1,None,None,None,None,1,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Ingeniería de Requerimientos de Software',
+        'cuatrimestre': 3,
+        'seriacion': [None,1,1,1,2,1,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Habilidades Cognitivas y Creatividad',
+        'cuatrimestre': 4,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 6
+    },
+    {
+        'nombre': 'Matemáticas para la Ingeniería I',
+        'cuatrimestre': 4,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Programación Web',
+        'cuatrimestre': 4,
+        'seriacion': [1,1,4,2,3,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Diseño de Interfaces',
+        'cuatrimestre': 4,
+        'seriacion': [None,None,None,1,None,None,None,None],
+        'holgura': 4
+    },
+    {
+        'nombre': 'Bases de Datos',
+        'cuatrimestre': 4,
+        'seriacion': [None,None,None,None,1,None,None,None],
+        'holgura': 5
+    },
+    {
+        'nombre': 'Electricidad y Magnetismo',
+        'cuatrimestre': 4,
+        'seriacion': [1,1,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Ética Profesional',
+        'cuatrimestre': 5,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 5
+    },
+    {
+        'nombre': 'Matemáticas para la Ingeniería II',
+        'cuatrimestre': 5,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 5
+    },
+    {
+        'nombre': 'Programación Cliente/Servidor',
+        'cuatrimestre': 5,
+        'seriacion': [1,3,1,3,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Fundamentos de Redes',
+        'cuatrimestre': 5,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 5
+    },
+    {
+        'nombre': 'Arquitectura de Software',
+        'cuatrimestre': 5,
+        'seriacion': [1,1,2,1,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Sistemas Digitales',
+        'cuatrimestre': 5,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Habilidades Gerenciales',
+        'cuatrimestre': 6,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 4
+    },
+    {
+        'nombre': 'Probabilidad y Estadística',
+        'cuatrimestre': 6,
+        'seriacion': [None,1,None,None,None,None,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Arquitectura de Computadoras',
+        'cuatrimestre': 6,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 4
+    },
+    {
+        'nombre': 'Redes',
+        'cuatrimestre': 6,
+        'seriacion': [1,None,1,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Calidad de Software',
+        'cuatrimestre': 6,
+        'seriacion': [1,2,1,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Estancia I',
+        'cuatrimestre': 6,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Liderazgo de Equipos de Alto Desempeño',
+        'cuatrimestre': 7,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 3
+    },
+    {
+        'nombre': 'Lenguajes y Autómatas',
+        'cuatrimestre': 7,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Sistemas Operativos',
+        'cuatrimestre': 7,
+        'seriacion': [None,1,None,None,None,None,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Programación Concurrente',
+        'cuatrimestre': 7,
+        'seriacion': [1,3,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Pruebas del Software',
+        'cuatrimestre': 7,
+        'seriacion': [2,1,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Estancia II',
+        'cuatrimestre': 7,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 3
+    },
+    {
+        'nombre': 'Programación para Móviles I',
+        'cuatrimestre': 8,
+        'seriacion': [2,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Compiladores e Intérpretes',
+        'cuatrimestre': 8,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Inteligencia Artificial',
+        'cuatrimestre': 8,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Análisis Financiero de Software',
+        'cuatrimestre': 8,
+        'seriacion': [1,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Mantenimiento de Software',
+        'cuatrimestre': 8,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Multimedia y Diseño Digital',
+        'cuatrimestre': 8,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 2
+    },
+    {
+        'nombre': 'Programación para Móviles II',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Seguridad de la Información',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Minería de Datos',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Administración de Proyectos de Software',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Arquitectura Orientada a Servicios',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+    {
+        'nombre': 'Expresión Oral y Escrita II',
+        'cuatrimestre': 9,
+        'seriacion': [None,None,None,None,None,None,None,None],
+        'holgura': 1
+    },
+]
